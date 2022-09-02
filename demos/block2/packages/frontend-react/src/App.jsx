2e73fac392be8326e8367f9d637c8a2c8a31e7ef
Oli Sturm <oliver@oliversturm.com>
2022-09-02 11:37:06 +0100

    initial

--- demos/block2/packages/frontend-react/src/App.jsx
+++ demos/block2/packages/frontend-react/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import AppFrame from './components/AppFrame';
 import CustomersView from './views/CustomersView';
+import AboutView from './views/AboutView';
 
 const App = () => {
   return (
     <div className="container mx-auto border-solid border-2 rounded-lg my-4 p-4 shadow-lg">
       <AppFrame>
-        <div>
+        <div className="border-solid border mt-4 rounded p-2">
+          {/* The React app does not route yet, since that feature
+              will be based on Redux later. But for testing
+              purposes, views can be swapped here manually. */}
           <CustomersView />
+          {/* <AboutView /> */}
         </div>
       </AppFrame>
     </div>
